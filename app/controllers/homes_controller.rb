800b1198084e15a7ac4f6a6970d8100a73d63f7a
Moeno-Takahashi <moooeno3takah@gmail.com>
2021-07-24 18:04:20 +0900

    [add]トップページ追加

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,10 @@
+class HomesController < ApplicationController
+  skip_before_action :require_login, only: [:index, :line_add_friends, :terms_of_use, :usage]
+  before_action :user_plan, only: [:step1, :step2, :step3, :step4, :step5, :advice]
+
+  def index
+    if logged_in?
+      redirect_to user_path
+    end
+  end
+end
