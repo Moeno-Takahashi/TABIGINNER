53017e6885a11eb67bdc2724a405e3e825303d89
Moeno-Takahashi <moooeno3takah@gmail.com>
2021-07-25 10:42:33 +0900

    [add]タスクリスト/カスタム機能追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,6 +2,7 @@ class TasksController < ApplicationController
   def user_tasks
       user_plan
       @task = Task.new
+      @my_task = current_user.tasks.my_task.includes([:user_tasks]) 
       @passport_task = Task.passport_task.includes([:user_tasks])
       @schedule_task = Task.schedule_task.includes([:user_tasks])
       @visa_task = Task.visa_task.includes([:user_tasks])
@@ -14,5 +15,29 @@ class TasksController < ApplicationController
       @hostel_task = Task.hostel_task.includes([:user_tasks])
       @belongings_task = Task.belongings_task.includes([:user_tasks])
       @app_task = Task.app_task.includes([:user_tasks])
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      flash[:success] = t('.success')
+      redirect_to user_tasks_path
+    else
+      flash[:danger] = t('.fail')
+      redirect_to user_tasks_path
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy! 
+    flash[:success] = t('.success')
+    redirect_to user_tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:user_id, :title, :body, :category_id)
   end
 end
