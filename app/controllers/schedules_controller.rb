6006f6df8409b9751eb74c81cfd3221d7a9019a7
Moeno-Takahashi <moe2cecil@icloud.com>
2021-08-20 10:41:35 +0900

    [fix]status管理の記述を修正

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -3,12 +3,17 @@ class SchedulesController < PlansController
   def edit; end
 
   def update
-    if @plan.update(plans_params)
-      if current_user.line_user.present?
+    if current_user.line_user.present?
+      if @plan.update(plans_params)
         redirect_to step5_home_path, success: t('.success')
       else
-        current_user.update!(status: 3)
+        render :edit
+      end
+    else
+      if @plan.update(plans_params) && current_user.update(status: 3)
         redirect_to step4_home_path, success: t('.success')
+      else
+        render :edit
       end
     end
   end
